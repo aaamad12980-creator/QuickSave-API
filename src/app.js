cb8236ccf5c819ca9c73f54ee2b23ba64df4bd52
aaamad12980-creator <aaamad12980@gmail.com>
2026-08-02 20:51:35 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const notFound = require('./middlewares/notFound.middleware');
 const errorHandler = require('./middlewares/errorHandler.middleware');
 
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(
   cors({
